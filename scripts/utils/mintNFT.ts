7dd56b4596b1f88ea9ae32d140c1cff2735e9a75
ggg3ya <153954117+ggg3ya@users.noreply.github.com>
2024-08-23 01:43:34 +0700

    Create mintNFT.ts

---
+++ scripts/utils/mintNFT.ts
@@ -0,0 +1,31 @@
+import { http, createWalletClient, createPublicClient } from 'viem'
+import { sepolia } from 'viem/chains'
+import { NFTContractAddress, RPCProviderUrl, account, mintContractApi } from './utils'
+
+export async function mintNFT(): Promise<string> {
+    console.log('Minting a new NFT...')
+    const walletClient = createWalletClient({
+        account,
+        chain: sepolia,
+        transport: http(RPCProviderUrl),
+    })
+    const publicClient = createPublicClient({
+        chain: sepolia,
+        transport: http(RPCProviderUrl),
+    })
+
+    const { request } = await publicClient.simulateContract({
+        address: NFTContractAddress,
+        functionName: 'mint',
+        args: [account.address],
+        abi: [mintContractApi],
+    })
+    const hash = await walletClient.writeContract(request)
+    console.log(Minted NFT successful with hash: ${hash})
+
+    const receipt = await publicClient.waitForTransactionReceipt({ hash })
+    const tokenId = Number(receipt.logs[0].topics[3]).toString()
+    console.log(Minted NFT tokenId: ${tokenId})
+
+    return tokenId
+}
